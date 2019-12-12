575d2f2288abe6016cecfe87aadffc23a5c019e7
sortymmith@outlook.com <sortymmith@outlook.com>
2019-12-12 16:28:50 -0600

    ReLU update with leaky ReLU

--- sources/relu.sv
+++ sources/relu.sv
@@ -11,6 +11,19 @@ Code Written by: Sorty MMith (sortymmith@outlook.com)
 15 Nov 2019
 */
 
+module leaky_relu #(parameter FRACTION_WIDTH=15, parameter BIT_WIDTH=32)
+(
+	input [BIT_WIDTH-1:0] in_x,
+	output [BIT_WIDTH-1:0] out_val
+);
+
+	// relu-> max(0.1x,x)
+	//if +ve, then in_x is output
+	// if -ve, then 0.1 means we shift right by 3 times (0.125*x)
+	assign out_val = in_x[BIT_WIDTH-1]?{1'b1,in_x[BIT_WIDTH-2:0]>>3}:in_x;
+
+endmodule
+
 module relu #(parameter FRACTION_WIDTH=15, parameter BIT_WIDTH=32)
 (
 	input [BIT_WIDTH-1:0] in_x,
@@ -20,6 +33,6 @@ module relu #(parameter FRACTION_WIDTH=15, parameter BIT_WIDTH=32)
 	// relu-> max(0.1x,x)
 	//if +ve, then in_x is output
 	// if -ve, then 0.1 means we shift right by 3 times (0.125*x)
-	assign out_val = in_x[BIT_WIDTH-1]?{1'b1,in_x[BIT_WIDTH-2:1]>>3}:in_x;
+	assign out_val = in_x[BIT_WIDTH-1]?{(BIT_WIDTH){1'b0}}:in_x;
 
 endmodule
